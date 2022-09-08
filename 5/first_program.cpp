611638af13635ec553a83b074e98d87e20d0f5a7
Alessandro Bisiani <alessandro.bisiani.1@gmail.com>
2022-09-08 15:08:27 +0200

    Initial commit. First syntax module.

---
+++ 5/first_program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+//using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+
+  int user_input;
+
+  cout << "Enter your favorite number 1-100: ";
+  cin >> user_input;
+  cout << "Amazing. " << user_input << " is my favorite number too!" << endl;
+
+  return 0;
+}
